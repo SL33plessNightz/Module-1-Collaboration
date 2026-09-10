f83e102c5c93bdf24448d45d6e1110ee28aa669a
SL33plessNightz <kingkennede10@gmail.com>
2026-09-10 10:49:56 -0500

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print("Hello world")
+60*60
+seconds_per_hour = 60*60
+seconds_per_hour * 24
+seconds_per_day = seconds_per_hour * 24
+pt3 = seconds_per_day / seconds_per_hour
+pt4 = seconds_per_day // seconds_per_hour
+print(pt3)
+print(pt4)
+print("Yea it agrees but pt 3.6 has a float instead of an int answer")
